04f78f0281db356476793e46ccc05d0feab0ef82
Harsh Upadhyay <harsh10.mitmpl2024@learner.manipal.edu>
2026-06-15 21:37:08 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int n = prices.size();
+        for(int i=1;i<n;i++){
+            if(prices[i-1] < prices[i])
+                profit += prices[i] - prices[i-1];
+        }
+        return profit;
+    }
+};
