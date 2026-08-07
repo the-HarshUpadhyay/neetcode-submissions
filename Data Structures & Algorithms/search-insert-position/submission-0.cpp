3a135ae891e383bc7422de9234fdd9899c3894b1
Harsh Upadhyay <harsh10.mitmpl2024@learner.manipal.edu>
2026-08-07 21:49:23 +0530

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size()-1;
+        int m = 0;
+        while(l<=r){
+            int m = l+ (r-l)/2;
+            if(nums[m] == target) return m;
+            else if(nums[m] > target) r = m-1;
+            else l = m+1;
+        }
+        return l;
+    }
+};
